f21dc3d9565706cc45c0df259c3e27608563c70b
Patrick <Patrick@DESKTOP-TN38UBG>
2024-10-18 19:54:54 +0800

    Add project files.

---
+++ SampleModel/Notes.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleModel
+{
+
+    public class Note
+    {
+        public int NoteId { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime DateModified { get; set; }
+        public int CategoryId { get; set; }
+        public Note()
+        {
+            DateCreated = DateTime.Now;
+            DateModified = DateTime.Now;
+        }
+    }
+
+}
